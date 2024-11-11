c0b7aa55e3c2f8200f838d9da779af5481a24277
pronobsarker23 <pronobsarker23@gmail.com>
2024-11-11 18:51:42 +0600

    js

---
+++ first.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    console.log("text not given");
+
+}
+
+myFunction();
+
+
+
+
+
+// let product = {
+//     pName: 'phone',
+//     model: 's25',
+//     price: 15000,
+// }
+
+// for (const result in product) {
+//     console.log(result);
+
+// }
